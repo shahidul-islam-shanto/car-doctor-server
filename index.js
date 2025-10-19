365761d15a03c46e66f4836f801cefb80a3d9e5d
shahidul-islam <shahidulislam47326@gmail.com>
2025-10-20 00:42:11 +0600

    booking update make

--- index.js
+++ index.js
@@ -71,6 +71,16 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.put("/bookings/:id", async (req, res) => {
+      const updateBooking = req.body;
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
