8a6ed7b4dcd44e0cb768f05399a6c12c6ebe8711
shahidul-islam <shahidulislam47326@gmail.com>
2025-10-20 21:55:15 +0600

    cred update

--- index.js
+++ index.js
@@ -78,8 +78,17 @@ async function run() {
       res.send(result);
     });
 
-    app.put("/bookings/:id", async (req, res) => {
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updateBooking = req.body;
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
