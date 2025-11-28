d8bce860ad79125310ef53a9a62db1e2faaa6a6c
shahidul-islam <shahidulislam47326@gmail.com>
2025-11-28 23:54:09 +0600

    url setup

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 // // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      // "http://localhost:5173"
+      "https://car-doctor-client-b927e.web.app",
+      "https://car-doctor-client-b927e.firebaseapp.com",
+    ],
     credentials: true,
   })
 );
