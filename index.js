78fc12dc75bd49831692261c5a081538d5b260d2
shahidul-islam <shahidulislam47326@gmail.com>
2025-12-02 21:07:42 +0600

    listen add

--- index.js
+++ index.js
@@ -1,192 +1,17 @@
-// const express = require("express");
-// const cors = require("cors");
-// const cookieParser = require("cookie-parser");
-// const jwt = require("jsonwebtoken");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// require("dotenv").config();
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// // // middleware
-// app.use(
-//   cors({
-//     origin: [
-//       // "http://localhost:5173",
-//       "https://car-doctor-client-b927e.web.app",
-//       "https://car-doctor-client-b927e.firebaseapp.com",
-//     ],
-//     credentials: true,
-//   })
-// );
-// app.use(express.json());
-// app.use(cookieParser());
-
-// // get
-// app.get("/", (req, res) => {
-//   res.send("this server is running");
-// });
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aazhdn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// /**custom middleware start */
-// // const logger = async (req, res, next) => {
-// //   console.log("called:", req.host, req.originalUrl);
-// //   next();
-// // };
-
-// const verifyToken = (req, res, next) => {
-//   const token = req?.cookies?.token;
-//   console.log("value of token middleware", token);
-
-//   if (!token) {
-//     return res.status(401).send({ message: "Not authorized" });
-//   }
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     // error
-//     if (err) {
-//       return res.status(401).send({ message: "unauthorized" });
-//     }
-//     // if token is valid then it would be decoded
-//     // console.log("value in the token", decoded);
-//     req.user = decoded;
-//     next();
-//   });
-// };
-// /**custom middleware end */
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-
-//     const serviceCollection = client.db("carDoctor").collection("services");
-//     const bookingCollection = client.db("carDoctor").collection("bookings");
-
-//     /** jwt token auth related api start */
-//     app.post("/jwt", async (req, res) => {
-//       const user = req.body;
-//       // console.log(user);
-//       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-//         expiresIn: "1h",
-//       });
-//       res
-//         .cookie("token", token, {
-//           httpOnly: true,
-//           secure: false,
-//           sameSite: "lax",
-//         })
-//         .send({ success: true });
-//     });
-
-//     // jwt logOut
-//     app.post("/logout", async (req, res) => {
-//       const user = req.body;
-//       res.clearCookie("token", { maxAge: 0 }).send({ success: true });
-//     });
-//     /** jwt token auth related api end */
-
-//     /** service related api start */
-//     app.get("/services", async (req, res) => {
-//       const cursor = serviceCollection.find();
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-
-//     app.get("/services/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const options = {
-//         projection: { title: 1, price: 1, service_id: 1, img: 1 },
-//       };
-//       const result = await serviceCollection.findOne(query, options);
-//       res.send(result);
-//     });
-
-//     // bookings
-//     app.post("/bookings", async (req, res) => {
-//       const booking = req.body;
-//       const result = await bookingCollection.insertOne(booking);
-//       res.send(result);
-//     });
-
-//     app.get("/bookings", verifyToken, async (req, res) => {
-//       console.log(req.query.email);
-//       // console.log("form valid token", req.user);
-//       if (req.query.email !== req.user.email) {
-//         return res.status(403).send({ massage: "forbidden access" });
-//       }
-//       // console.log("tok tok token", req.cookies.token);
-//       let query = {};
-//       if (req.query?.email) {
-//         query = { email: req.query.email }; //set email
-//       }
-//       const result = await bookingCollection.find(query).toArray();
-//       res.send(result);
-//     });
-
-//     app.delete("/bookings/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const result = await bookingCollection.deleteOne(query);
-//       res.send(result);
-//     });
-
-//     app.patch("/bookings/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const filter = { _id: new ObjectId(id) };
-//       const updateBooking = req.body;
-//       const updateDoc = {
-//         $set: {
-//           status: updateBooking.status,
-//         },
-//       };
-//       const result = await bookingCollection.updateOne(filter, updateDoc);
-//       res.send(result);
-//     });
-//     /** service related api end */
-
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// // app.listen(port, () => {
-// //   console.log(`Example app listening on port ${port}`);
-// // });
-
-// module.exports = app;
-
-//**================================================= */
-
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-
 const app = express();
+const port = process.env.PORT || 5000;
 
-// middleware
+// // middleware
 app.use(
   cors({
     origin: [
+      // "http://localhost:5173",
       "https://car-doctor-client-b927e.web.app",
       "https://car-doctor-client-b927e.firebaseapp.com",
     ],
@@ -196,150 +21,326 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-// Root route
+// get
 app.get("/", (req, res) => {
-  res.send("Car Doctor server is running on Vercel!");
+  res.send("this server is running");
 });
 
-// MongoDB connection (Serverless SAFE)
-let client;
-async function connectDB() {
-  if (!client) {
-    client = new MongoClient(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aazhdn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
-      {
-        serverApi: {
-          version: ServerApiVersion.v1,
-          strict: true,
-          deprecationErrors: true,
-        },
-      }
-    );
-    await client.connect();
-  }
-  return client;
-}
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aazhdn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// JWT verify middleware
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+/**custom middleware start */
+// const logger = async (req, res, next) => {
+//   console.log("called:", req.host, req.originalUrl);
+//   next();
+// };
+
 const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
+  console.log("value of token middleware", token);
 
   if (!token) {
     return res.status(401).send({ message: "Not authorized" });
   }
-
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    // error
     if (err) {
       return res.status(401).send({ message: "unauthorized" });
     }
+    // if token is valid then it would be decoded
+    // console.log("value in the token", decoded);
     req.user = decoded;
     next();
   });
 };
+/**custom middleware end */
 
-// API routes
-app.post("/jwt", async (req, res) => {
-  const user = req.body;
-  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "1h",
-  });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-  res
-    .cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-    })
-    .send({ success: true });
-});
+    const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
 
-app.post("/logout", async (req, res) => {
-  res.clearCookie("token", { maxAge: 0 }).send({ success: true });
-});
+    /** jwt token auth related api start */
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: "lax",
+        })
+        .send({ success: true });
+    });
 
-// GET all services
-app.get("/services", async (req, res) => {
-  const clientDB = await connectDB();
-  const serviceCollection = clientDB.db("carDoctor").collection("services");
+    // jwt logOut
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
+    /** jwt token auth related api end */
 
-  const result = await serviceCollection.find().toArray();
-  res.send(result);
-});
+    /** service related api start */
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-// GET single service
-app.get("/services/:id", async (req, res) => {
-  const id = req.params.id;
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
 
-  const clientDB = await connectDB();
-  const serviceCollection = clientDB.db("carDoctor").collection("services");
+    // bookings
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
 
-  const result = await serviceCollection.findOne(
-    { _id: new ObjectId(id) },
-    {
-      projection: { title: 1, price: 1, img: 1, service_id: 1 },
-    }
-  );
+    app.get("/bookings", verifyToken, async (req, res) => {
+      console.log(req.query.email);
+      // console.log("form valid token", req.user);
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ massage: "forbidden access" });
+      }
+      // console.log("tok tok token", req.cookies.token);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }; //set email
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
 
-  res.send(result);
-});
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
 
-// POST booking
-app.post("/bookings", async (req, res) => {
-  const booking = req.body;
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateBooking = req.body;
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    /** service related api end */
 
-  const clientDB = await connectDB();
-  const bookingCollection = clientDB.db("carDoctor").collection("bookings");
-
-  const result = await bookingCollection.insertOne(booking);
-  res.send(result);
-});
-
-// GET bookings (protected)
-app.get("/bookings", verifyToken, async (req, res) => {
-  if (req.query.email !== req.user.email) {
-    return res.status(403).send({ message: "forbidden access" });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
+}
+run().catch(console.dir);
 
-  const clientDB = await connectDB();
-  const bookingCollection = clientDB.db("carDoctor").collection("bookings");
-
-  const result = await bookingCollection
-    .find({ email: req.query.email })
-    .toArray();
-
-  res.send(result);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
 
-// DELETE booking
-app.delete("/bookings/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const clientDB = await connectDB();
-  const bookingCollection = clientDB.db("carDoctor").collection("bookings");
-
-  const result = await bookingCollection.deleteOne({
-    _id: new ObjectId(id),
-  });
-
-  res.send(result);
-});
-
-// PATCH booking
-app.patch("/bookings/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const clientDB = await connectDB();
-  const bookingCollection = clientDB.db("carDoctor").collection("bookings");
-
-  const result = await bookingCollection.updateOne(
-    { _id: new ObjectId(id) },
-    {
-      $set: { status: req.body.status },
-    }
-  );
-
-  res.send(result);
-});
-
-// IMPORTANT for Vercel — do NOT use app.listen()
 module.exports = app;
+
+//**================================================= */
+
+// const express = require("express");
+// const cors = require("cors");
+// const cookieParser = require("cookie-parser");
+// const jwt = require("jsonwebtoken");
+// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// require("dotenv").config();
+
+// const app = express();
+
+// // middleware
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       // "https://car-doctor-client-b927e.web.app",
+//       // "https://car-doctor-client-b927e.firebaseapp.com",
+//     ],
+//     credentials: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(cookieParser());
+
+// // Root route
+// app.get("/", (req, res) => {
+//   res.send("Car Doctor server is running on Vercel!");
+// });
+
+// // MongoDB connection (Serverless SAFE)
+// let client;
+// async function connectDB() {
+//   if (!client) {
+//     client = new MongoClient(
+//       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aazhdn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+//       {
+//         serverApi: {
+//           version: ServerApiVersion.v1,
+//           strict: true,
+//           deprecationErrors: true,
+//         },
+//       }
+//     );
+//     await client.connect();
+//   }
+//   return client;
+// }
+
+// // JWT verify middleware
+// const verifyToken = (req, res, next) => {
+//   const token = req?.cookies?.token;
+
+//   if (!token) {
+//     return res.status(401).send({ message: "Not authorized" });
+//   }
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "unauthorized" });
+//     }
+//     req.user = decoded;
+//     next();
+//   });
+// };
+
+// // API routes
+// app.post("/jwt", async (req, res) => {
+//   const user = req.body;
+//   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+//     expiresIn: "1h",
+//   });
+
+//   res
+//     .cookie("token", token, {
+//       httpOnly: true,
+//       secure: true,
+//       sameSite: "none",
+//     })
+//     .send({ success: true });
+// });
+
+// app.post("/logout", async (req, res) => {
+//   res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+// });
+
+// // GET all services
+// app.get("/services", async (req, res) => {
+//   const clientDB = await connectDB();
+//   const serviceCollection = clientDB.db("carDoctor").collection("services");
+
+//   const result = await serviceCollection.find().toArray();
+//   res.send(result);
+// });
+
+// // GET single service
+// app.get("/services/:id", async (req, res) => {
+//   const id = req.params.id;
+
+//   const clientDB = await connectDB();
+//   const serviceCollection = clientDB.db("carDoctor").collection("services");
+
+//   const result = await serviceCollection.findOne(
+//     { _id: new ObjectId(id) },
+//     {
+//       projection: { title: 1, price: 1, img: 1, service_id: 1 },
+//     }
+//   );
+
+//   res.send(result);
+// });
+
+// // POST booking
+// app.post("/bookings", async (req, res) => {
+//   const booking = req.body;
+
+//   const clientDB = await connectDB();
+//   const bookingCollection = clientDB.db("carDoctor").collection("bookings");
+
+//   const result = await bookingCollection.insertOne(booking);
+//   res.send(result);
+// });
+
+// // GET bookings (protected)
+// app.get("/bookings", verifyToken, async (req, res) => {
+//   if (req.query.email !== req.user.email) {
+//     return res.status(403).send({ message: "forbidden access" });
+//   }
+
+//   const clientDB = await connectDB();
+//   const bookingCollection = clientDB.db("carDoctor").collection("bookings");
+
+//   const result = await bookingCollection
+//     .find({ email: req.query.email })
+//     .toArray();
+
+//   res.send(result);
+// });
+
+// // DELETE booking
+// app.delete("/bookings/:id", async (req, res) => {
+//   const id = req.params.id;
+
+//   const clientDB = await connectDB();
+//   const bookingCollection = clientDB.db("carDoctor").collection("bookings");
+
+//   const result = await bookingCollection.deleteOne({
+//     _id: new ObjectId(id),
+//   });
+
+//   res.send(result);
+// });
+
+// // PATCH booking
+// app.patch("/bookings/:id", async (req, res) => {
+//   const id = req.params.id;
+
+//   const clientDB = await connectDB();
+//   const bookingCollection = clientDB.db("carDoctor").collection("bookings");
+
+//   const result = await bookingCollection.updateOne(
+//     { _id: new ObjectId(id) },
+//     {
+//       $set: { status: req.body.status },
+//     }
+//   );
+
+//   res.send(result);
+// });
+
+// // IMPORTANT for Vercel — do NOT use app.listen()
+// module.exports = app;
