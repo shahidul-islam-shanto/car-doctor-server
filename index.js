8fec9b4de5cfa78beb90ecb6a94f460c6afb640c
shahidul-islam <shahidulislam47326@gmail.com>
2025-12-02 22:02:24 +0600

    new vercel.json file add

--- index.js
+++ index.js
@@ -221,16 +221,7 @@ module.exports = app;
 // //   return client;
 // // }
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aazhdn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
 
 // // JWT verify middleware
 // const verifyToken = (req, res, next) => {
