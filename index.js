53634d6e997a0b2998503572b5e5364578cdcf44
shahidul-islam <shahidulislam47326@gmail.com>
2025-12-03 20:30:12 +0600

    copy update

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.use(
   cors({
     origin: [
       // "http://localhost:5173",
-      "https://car-doctor-client-b927e.web.app/",
-      "https://car-doctor-client-b927e.firebaseapp.com/",
-      "https://car-cure-client.vercel.app/",
+      "https://car-doctor-client-b927e.web.app",
+      "https://car-doctor-client-b927e.firebaseapp.com",
+      "https://car-cure-client.vercel.app",
     ],
     credentials: true,
   })
